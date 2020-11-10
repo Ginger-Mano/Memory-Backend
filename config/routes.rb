9572c08e01cf8ddabe52d00be3d88e0e65a88958
kelseyjdickson <kelseymedved@gmail.com>
2020-11-10 09:31:24 -0500

    completed first video on sessions based rails api auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :games
   resources :cards
   resources :users
+  get "/temp", to:"application#temp"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
